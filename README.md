efbedaa51f9204da2e9e5878bf8577bb45d33265
firesurfer <firesurfer65@yahoo.de>
2016-09-03 18:44:27 +0200

    Added serveral examples

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Please make sure you did the steps described in the rclcs documentation before u
 ## What does this workspace consist of?
 
 * src/test_msgs   -  A simple test message which will be translated to c/c++ and c# code
-* src/test_cs	  -  Test and demonstration that shows how to use the rclcs in order to communicate via ROS2
+* src/test_cs	  -  Test and demonstration that shows how to use the rclcs in order to communicate via ROS2 (This is the C# code)
 * src/test_cpp    - Cpp listener for the message
 * src/test_cpp_pub - Cpp publisher for the message 
 
@@ -21,3 +21,12 @@ cd install/bin
 export MONO_PATH=../lib:<Path to ros2 workspace/install/lib>
 mono test_cs.exe
 ```
+
+This will start the Main function defined in Program.cs
+
+For commented examples have a look at `test_cs/test_cs/Examples`.
+There are three examples at the moment.
+
+* BasicNodeExample - demonstrates how to initialize the rcl and how to create/spin a node
+* PublisherExample - create a publisher an publish the dummy message
+* SubscriptionExample - subscribe on a TestTopic an recieve the Dummy message
---
+++ src/test_cs/test_cs/Examples/PublisherExample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using rclcs;
+
+namespace test_cs
+{
+	/**
+	 * This example shows how to create a publisher
+	 */
+	public class PublisherExample
+	{
+		public static void Main (string[] args)
+		{
+			//Create instance of RCL class which handles functions from rcl.h
+			//RCL implements IDisposable so the using statement makes sure rcl_shutdown will be called after usages
+			using (RCL rcl = new RCL ()) {
+				//Initialise RCL with default allocator
+				rcl.Init (args);
+
+				//Create an executor that will task our node
+				Executor demoExecutor = new SingleThreadedExecutor ();
+				//Let the executor task all nodes with the given timespan
+				demoExecutor.Spin (new TimeSpan (0, 0, 0, 0, 10));
+				//Create a Node
+				using (Node testNode = new Node ("BasicNodeExample")) {
+					//Add node to executor
+					demoExecutor.AddNode (testNode);
+
+					//Now we're creating a publisher with the Dummy message
+					//TODO show alternative to Node.CreatePublisher<T>
+					using (Publisher<test_msgs.msg.Dummy> testPublisher = testNode.CreatePublisher<test_msgs.msg.Dummy> ("TestTopic")) {
+						//Create a message //TODO let message implement IDisposable
+						test_msgs.msg.Dummy testMsg = new test_msgs.msg.Dummy (); 
+						//Fill the message fields
+						testMsg.thisafloat32 = 0.4f;
+						//Fill a string //TODO -> Make strings better usable
+						testMsg.thisisastring = new rosidl_generator_c__String ("TestString");
+						//Fill an array
+						testMsg.thisafloat32array = new rosidl_generator_c__primitive_array_float32 (new float[]{ 1.3f, 100000.4f });
+
+						//And now publish the message
+						testPublisher.Publish (testMsg);
+
+					}
+
+				}
+				//Remember to stop the executor - you could start it again afterwards
+				demoExecutor.Cancel ();
+
+			}
+			//rcl_shutdown gets called automatically
+		}
+	}
+}
+
